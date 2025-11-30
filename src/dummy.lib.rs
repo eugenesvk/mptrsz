d9b044e6b434747b13939cac815cbe1fef642898
eugenesvk <eugenesvk@users.noreply.github.com>
2025-12-01 01:30:10 +0700

    move get_mptr_sz to the library

--- src/dummy.lib.rs
+++ src/dummy.lib.rs
@@ -1,5 +1,5 @@
-#![cfg_attr(not(debug_assertions),allow(non_snake_case,non_upper_case_globals,non_camel_case_types))]
-#![cfg_attr(    debug_assertions ,allow(non_snake_case,non_upper_case_globals,non_camel_case_types,unused_imports,unused_mut,unused_variables,dead_code,unused_assignments,unused_macros))]
+#![cfg_attr(not(debug_assertions),allow(uncommon_codepoints,non_snake_case,non_upper_case_globals,non_camel_case_types))]
+#![cfg_attr(    debug_assertions ,allow(uncommon_codepoints,non_snake_case,non_upper_case_globals,non_camel_case_types,unused_imports,unused_mut,unused_variables,dead_code,unused_assignments,unused_macros))]
 extern crate helperes      as h    ;
 extern crate helperes_proc as hproc;
 use ::h            	::*; // gets macros :: prefix needed due to proc macro expansion
@@ -8,8 +8,13 @@ pub use ::h::alias 	::*;
 pub use ::h::helper	::*;
 
 #[path="libmod/[libmod].rs"] pub mod libmod;
-use crate::libmod::ret42;
+use crate::libmod::{ret42,get_mptr_sz};
 
 pub fn lib() -> i32 {
+  let coords = get_mptr_sz(None);
+  // match coords {
+    // Some(c)	=> {println!("coords {:?}",c);},
+    // None   	=> {println!("no mouse pointer shape captured");},
+  // };
   ret42()
 }
