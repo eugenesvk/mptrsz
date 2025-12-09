cfafa31e15f9a9296024c7aa436b420ee2d0ac68
eugenesvk <eugenesvk@users.noreply.github.com>
2025-12-10 03:09:59 +0700

    comment

--- src/libmod/capture.rs
+++ src/libmod/capture.rs
@@ -145,7 +145,7 @@ use std::mem::{size_of,zeroed};
 
 
 #[docpos]
-pub fn get_mptr_sz( /// Get the true bounding box of a 🖰 pointer (if visible), i.e., the minimal box that contains all the pointer pixels. If `E̲nable pointer shadow` Windows Mouse setting is on, the cursor size increases by ~9⋅7 pixels, so instead of 48⋅48 you'd get 57⋅55 (also affects the cursor positioning within the cursor frame). `GetCursorInfo` alternative seems to ignore shadows and always gets 48⋅48. However, `ColorMasked` cursors (24𝑏=8𝑏⋅3𝑐 `TrueColor` colors with at least 1 pixel "inverted" that requires using α-channel to track inversion (0xFF inverts, 0x0 replaces; 𝑎-channel is 0-ed out in regular 24𝑏 color bitmap)) do not drop shadow, so retain the same size (48⋅48 in the example above)
+pub fn get_mptr_sz( /// Get the true bounding box of a 🖰 pointer (if visible), i.e., the minimal box that contains all the pointer pixels. If `E̲nable pointer shadow` Windows Mouse setting is on, the cursor size increases by ~9⋅7 pixels, so instead of 48⋅48 you'd get 57⋅55 (also affects the cursor positioning within the cursor frame). `GetCursorInfo` alternative seems to ignore shadows and always gets 48⋅48. However, `Colorμ` cursors (24𝑏=8𝑏⋅3𝑐 `TrueColor` colors with at least 1 pixel "inverted" that requires using α-channel to track inversion (0xFF inverts, 0x0 replaces; 𝑎-channel is 0-ed out in regular 24𝑏 color bitmap)) do not drop shadow, so retain the same size (48⋅48 in the example above)
   ///! store the text drawing of the pointer and print a few metrics (mostly for debugging)
   mut s:Option<&mut String>
 ) -> Option<cur_box>  {
@@ -175,7 +175,7 @@ pub fn get_mptr_sz( /// Get the true bounding box of a 🖰 pointer (if visible)
 
       let mut scan_line_test     = 0;
       let mut chunk_test:Vec<u8> = vec![];
-      // !: empty pointer will have nonsensical →0 < ←w, this is not checked    ■•◧□
+      // !: empty pointer will have nonsensical →0 < ←w, this is not checked    █■•◧□
       let mut most𐎓	= w as usize; //pushed ← if a valid pixel found
       let mut most𑁱	= 0         ; //pushed → …
       let mut most𖭩	= h as usize;
