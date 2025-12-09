a23957def6f1a09cc51611127ff157ebb216dfa8
eugenesvk <eugenesvk@users.noreply.github.com>
2025-12-10 03:09:20 +0700

    add mods

--- src/libmod/[libmod].rs
+++ src/libmod/[libmod].rs
@@ -1,1 +1,14 @@
+mod error;
+mod ext;
+mod capture;
+mod measure;
+
+mod error_test;
+mod capture_test;
+
+pub use error::*;
+pub use capture::*;
+pub use measure::*;
+pub use ext::*;
+
 pub fn ret42() -> i32 { 42 }
