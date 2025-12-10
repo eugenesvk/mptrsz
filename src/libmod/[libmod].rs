740ee205f92265cb7e0bfa84269967ec88c617ef
eugenesvk <eugenesvk@users.noreply.github.com>
2025-12-11 04:58:29 +0700

    fix macro definitions and reformat
    use cfg!, regular cfg was for attributes (though seemed to work?)

--- src/libmod/[libmod].rs
+++ src/libmod/[libmod].rs
@@ -13,19 +13,12 @@ pub use ext::*;
 
 // println! conditionally depending on φL level
 const φL:u8 = 3;
-
-#[macro_export] macro_rules! φ  {($($tokens:tt)*) => {#[cfg(    debug_assertions)]
-            println!("{}",format!( $($tokens)*));     #[cfg(not(debug_assertions))]{}    }}
-#[macro_export] macro_rules! φ1 {($($tokens:tt)*) => {#[cfg(    debug_assertions)]
-  if φL>=1{println!("{}",format!( $($tokens)*))}else{}#[cfg(not(debug_assertions))]{}    }}
-#[macro_export] macro_rules! φ2 {($($tokens:tt)*) => {#[cfg(    debug_assertions)]
-  if φL>=2{println!("{}",format!( $($tokens)*))}else{}#[cfg(not(debug_assertions))]{}    }}
-#[macro_export] macro_rules! φ3 {($($tokens:tt)*) => {#[cfg(    debug_assertions)]
-  if φL>=3{println!("{}",format!( $($tokens)*))}else{}#[cfg(not(debug_assertions))]{}    }}
-#[macro_export] macro_rules! φ4 {($($tokens:tt)*) => {#[cfg(    debug_assertions)]
-  if φL>=4{println!("{}",format!( $($tokens)*))}else{}#[cfg(not(debug_assertions))]{}    }}
-#[macro_export] macro_rules! φ5 {($($tokens:tt)*) => {#[cfg(    debug_assertions)]
-  if L5>=4{println!("{}",format!( $($tokens)*))}else{}#[cfg(not(debug_assertions))]{}    }}
+#[macro_export] macro_rules! φ {($($tokens:tt)*) => {if cfg!(debug_assertions){          pp!("{}",format!($($tokens)*))         } else{} }}
+#[macro_export] macro_rules! φ1{($($tokens:tt)*) => {if cfg!(debug_assertions){ if φL>=1{pp!("{}",format!($($tokens)*))} else {}} else{} }}
+#[macro_export] macro_rules! φ2{($($tokens:tt)*) => {if cfg!(debug_assertions){ if φL>=2{pp!("{}",format!($($tokens)*))} else {}} else{} }}
+#[macro_export] macro_rules! φ3{($($tokens:tt)*) => {if cfg!(debug_assertions){ if φL>=3{pp!("{}",format!($($tokens)*))} else {}} else{} }}
+#[macro_export] macro_rules! φ4{($($tokens:tt)*) => {if cfg!(debug_assertions){ if φL>=4{pp!("{}",format!($($tokens)*))} else {}} else{} }}
+#[macro_export] macro_rules! φ5{($($tokens:tt)*) => {if cfg!(debug_assertions){ if φL>=5{pp!("{}",format!($($tokens)*))} else {}} else{} }}
 use φ as φ0;
 
 pub fn ret42() -> i32 { 42 }
