b261347638308fb09aad6322e465900a1a31ae64
eugenesvk <eugenesvk@users.noreply.github.com>
2025-12-11 02:12:44 +0700

    add debug prints that are compiled only in debug mode, and then depend on log level φL

--- src/libmod/[libmod].rs
+++ src/libmod/[libmod].rs
@@ -11,4 +11,21 @@ pub use capture::*;
 pub use measure::*;
 pub use ext::*;
 
+// println! conditionally depending on φL level
+const φL:u8 = 3;
+
+#[macro_export] macro_rules! φ  {($($tokens:tt)*) => {#[cfg(    debug_assertions)]
+            println!("{}",format!( $($tokens)*));     #[cfg(not(debug_assertions))]{}    }}
+#[macro_export] macro_rules! φ1 {($($tokens:tt)*) => {#[cfg(    debug_assertions)]
+  if φL>=1{println!("{}",format!( $($tokens)*))}else{}#[cfg(not(debug_assertions))]{}    }}
+#[macro_export] macro_rules! φ2 {($($tokens:tt)*) => {#[cfg(    debug_assertions)]
+  if φL>=2{println!("{}",format!( $($tokens)*))}else{}#[cfg(not(debug_assertions))]{}    }}
+#[macro_export] macro_rules! φ3 {($($tokens:tt)*) => {#[cfg(    debug_assertions)]
+  if φL>=3{println!("{}",format!( $($tokens)*))}else{}#[cfg(not(debug_assertions))]{}    }}
+#[macro_export] macro_rules! φ4 {($($tokens:tt)*) => {#[cfg(    debug_assertions)]
+  if φL>=4{println!("{}",format!( $($tokens)*))}else{}#[cfg(not(debug_assertions))]{}    }}
+#[macro_export] macro_rules! φ5 {($($tokens:tt)*) => {#[cfg(    debug_assertions)]
+  if L5>=4{println!("{}",format!( $($tokens)*))}else{}#[cfg(not(debug_assertions))]{}    }}
+use φ as φ0;
+
 pub fn ret42() -> i32 { 42 }
