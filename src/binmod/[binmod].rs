dc3b9ebc8f3f691cc6e6c59f717186086b5911a6
eugenesvk <eugenesvk@users.noreply.github.com>
2025-12-13 02:37:34 +0700

    remove empty example docpos

--- src/binmod/[binmod].rs
+++ src/binmod/[binmod].rs
@@ -11,14 +11,3 @@ use std::result;
 
 type Result<T> = result::Result<T, Box<dyn Error>>;
 pub fn print42() -> Result<()> {p!("{}",42)?; Ok(())}
-
-use std::path::PathBuf;
-use docpos::*;
-
-#[docpos] pub struct StructyPos { /// "inner" scruct docs
-  pub field1       :        String  ,/// pos-doc for `field1` (in regular Rust this would be a doc for `field2_longer`)
-  pub field2_longer: Option<String> ,/// pos-doc for `field2_longer`
-                                     /// pos-doc for `field2_longer` line 2
-                                     ///! pre-doc for `paths` at `field2_longer` (after `///!`)
-  pub paths        : Vec   <PathBuf>, // no doc comments allowed here, use `///!` in the previous field
-}
