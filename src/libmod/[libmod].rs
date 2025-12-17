a7f850a0a8e8d85602382005b4d392d3e04649c2
eugenesvk <eugenesvk@users.noreply.github.com>
2025-12-18 06:56:12 +0700

    lib: add get_cursor_sz

--- src/libmod/[libmod].rs
+++ src/libmod/[libmod].rs
@@ -3,6 +3,7 @@ mod ext;
 mod common;
 mod capture;
 mod measure;
+mod get_cursor_sz;
 
 mod error_test;
 mod capture_test;
@@ -11,6 +12,7 @@ pub use error::*;
 pub use common::*;
 pub use capture::*;
 pub use measure::*;
+pub use get_cursor_sz::*;
 pub use ext::*;
 
 pub fn ret42() -> i32 { 42 }
