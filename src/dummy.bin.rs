50a7cd576f78e97d0293f4c57cc330efb53b0cf0
eugenesvk <eugenesvk@users.noreply.github.com>
2025-12-18 06:59:50 +0700

    move some cursor functions from exe to lib

--- src/dummy.bin.rs
+++ src/dummy.bin.rs
@@ -9,10 +9,10 @@ pub use ::h::helper	::*;
 
 _mod!(binmod); //→ #[path="binmod/[binmod].rs"] pub mod binmod;
 use crate::binmod::print42;
-use mouse_sz_lib::libmod::{ret42,get_mptr_sz,measure_mcursor_bm,cur_box,Point,};
+use mouse_sz_lib::libmod::{ret42,get_mptr_sz,measure_mcursor_bm,cur_box,Point,parse_cursor_h,parse_cursor_dxgi};
 use mouse_sz_lib::φ;
 
-use std::error::Error;
+use thiserror::Error;
 use std::result;
 use std::mem;
 use std::mem::{size_of, zeroed};
@@ -34,46 +34,8 @@ use windows::Win32::UI::WindowsAndMessaging::{GetCursor, GetCursorPos, GetCursor
 
 use std::path::PathBuf;
 use docpos::*;
-use mouse_sz_lib::libmod::CursorColor;
 
-fn parse_cursor_h(cur_h:HCURSOR) -> Option<cur_box> {
-  let mut iℹ = ICONINFO::default();
-    /*fIcon :BOOL   	TRUE specifies an icon; FALSE specifies a cursor
-    xHotspot:u32    	, yHotspot:u32
-    hbmMask :hBitMap	icon monochrome mask bitmap. Monochrome icons: hbmMask = 2⋅iconHeight = AND mask on top and XOR mask on the bottom
-    hbmColor:hBitMap	icon color           bitmap. NULL for monochrome*/
-  // todo: convert to a proper error
-  let res = unsafe { GetIconInfo(cur_h.into(), &mut iℹ) }; if !res.is_ok() {pp!("1) ✗ GetIconInfo");None}else{
-    let iℹ_T 	= if iℹ.fIcon == TRUE {'🖼'}else{'🖰'};
-    let hot_x	=    iℹ.xHotspot; let hot_y = iℹ.yHotspot;
-    φ!("2) T={iℹ_T} {}  hot_x{hot_x} y{hot_y} (GetIconInfo)",if iℹ_T=='🖰'{"≝🖰"}else{"!!! should be 🖰 !!!"});
-    let mut hot_p = Point {x:iℹ.xHotspot as i32, y:iℹ.yHotspot as i32};
-
-    // 3 Get handle(s) to the cursor bitmap mask(s)
-    let coords = if dbg {let mut out_str = String::new();
-      let _r	=measure_mcursor_bm(iℹ.hbmMask, iℹ.hbmColor, hot_p, Some(&mut out_str)); pp!("{}",out_str); _r
-    } else  	{measure_mcursor_bm(iℹ.hbmMask, iℹ.hbmColor, hot_p, None              )};
-    // let bm_h = if iℹ.hbmColor.is_invalid() {iℹ.hbmMask} else {iℹ.hbmColor};
-    // test_GetDIBits(bm_h);
-
-    // Avoid resource leaks    DeleteObject(ho:HGDIOBJ) -> BOOL
-    let _d1 = if iℹ.hbmMask .is_invalid(){TRUE}else{unsafe{DeleteObject(iℹ.hbmMask .into())}};
-    let _d2 = if iℹ.hbmColor.is_invalid(){TRUE}else{unsafe{DeleteObject(iℹ.hbmColor.into())}};
-    if _d1==FALSE || _d2==FALSE {pp!("🛑GDI resource leak! ✗Mask {_d1:?} ✗Color {_d2:?}");}
-
-    coords
-  }
-}
-
-fn parse_cursor_dxgi() -> Option<cur_box> {
-  if dbg {pp!("\n\n\n——————————————— 2. DXGI duplication API\n");}
-  if dbg {let mut out_str = String::new();
-    let _r	=get_mptr_sz(Some(&mut out_str)); pp!("{}",out_str); _r
-  } else  	{get_mptr_sz(None)}
-}
-
-
-fn main() {
+pub fn main() {
   // TODO: when cursor is invisible, use alternative method of measuring its size
     // system metrics? add enum in return type to know: ≝, bitmap parsing, 3rd???
     // if ( !size.x ) { // use default icon size on this hardware
@@ -104,7 +66,7 @@ fn main() {
     if curℹ.flags != CURSOR_SHOWING {return}
 
     // 1.2 Get handle(s) to the cursor bitmap mask(s)
-    let coords = parse_cursor_h(cur_h);
+    let coords = parse_cursor_h(cur_h, true);
     match coords {
       Ok(c) 	=> {pp!("global 🖰 𝑏map: coords {:?}",c);},
       Err(e)	=> {pp!("global 🖰 𝑏map: no mouse pointer shape captured: {e}");},
@@ -129,25 +91,5 @@ fn main() {
       Ok (c)	=> {pp!("DXGI: coords {:?}",c);},
       Err(𝑒)	=> {pp!("DXGI: no mouse pointer shape captured: {}",𝑒);},
     };
-  //
-  // TODO: HOW to detect whether a cursor is 24bit color (all α=0 even though it exists) or 32 bit color (α
-      // no, an empty cursor has all α=0
-    // is it safe to assume if no α channel exists, then it must be 32bit color?
-      // 𝑎 always exists as part of the bitmap, the question is which values it supports
-  //
   // TODO: parse bitmap from this handle
-  // todo: how to get mask size with shadow like DXGI does?
-    // dxgi: how to ignore shadow and get only the size of the cursor itself?
-
-  // dxdiag outputs actual pointer size, whie geticoninfo only gets I-beam bitmap size?
-
 }
-
-
-use core::ffi::{c_void,c_int,};
-use std::ptr::null_mut;
-use windows::Win32::Graphics::Gdi::{HDC};
-use windows::Win32::Graphics::Gdi::{GetDC};
-
-
-
