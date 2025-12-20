38f231032c2bfe0a76c93a53c93bfbe97a9b6d0d
eugenesvk <eugenesvk@users.noreply.github.com>
2025-12-20 20:28:29 +0700

    erro: add an example of non-str numeric-based enums

--- src/libmod/error.rs
+++ src/libmod/error.rs
@@ -15,3 +15,25 @@ pub enum CursorSizeErr {
   #[error("🛑GDI resource leak!")]
   ResourceLeak(String),
 }
+
+/*
+use std::num::NonZeroU8;
+#[repr(transparent)] //allows returning in FFI since its layout = u8
+pub struct ErrC(NonZeroU8);
+
+// const TEN: NonZeroU8 = NonZeroU8::new(10).expect("ten is non-zero");
+const DXDupe      	: ErrC = ErrC(NonZeroU8::new(1).expect("1 ≠ 0"));
+const Bitmap      	: ErrC = ErrC(NonZeroU8::new(2).expect("2 ≠ 0"));
+const Ii          	: ErrC = ErrC(NonZeroU8::new(3).expect("3 ≠ 0"));
+const BoxSzInvalid	: ErrC = ErrC(NonZeroU8::new(4).expect("4 ≠ 0"));
+const ResourceLeak	: ErrC = ErrC(NonZeroU8::new(5).expect("5 ≠ 0"));
+
+#[unsafe(no_mangle)] pub extern "C"
+fn test_error_code1() -> Option<ErrC> {
+  Some(DXDupe)
+}
+#[unsafe(no_mangle)] pub extern "C"
+fn test_error_code0() -> Option<ErrC> {
+  None
+}
+*/
